f4457406e085d5440f248650f2b6b528b7c22361
ap3549746 <ap3549746@gmail.com>
2026-06-18 12:21:21 +0530

    ternaryOperator

---
+++ src/day3/ternaryOperator.java
@@ -0,0 +1,9 @@
+package day3;
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int Number = 10;
+        String Type  = (Number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(Type);
+    }
+}
