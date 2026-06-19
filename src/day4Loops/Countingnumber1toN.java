2339067695438fb8227292a40b895416f3cded40
ap3549746 <ap3549746@gmail.com>
2026-06-19 10:32:22 +0530

    Countingnumber1toN

---
+++ src/day4Loops/Countingnumber1toN.java
@@ -0,0 +1,16 @@
+package day4Loops;
+import java.util.*;
+public class Countingnumber1toN {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of N:-9");
+        int N = sc.nextInt();
+        int counter=1;
+        while (counter<=N){
+            System.out.print(counter+" ");
+            counter++;
+        }
+        System.out.println();
+
+    }
+}
