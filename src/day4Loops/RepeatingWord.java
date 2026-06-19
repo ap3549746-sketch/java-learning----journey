57952a5911851a6a7abe8830a05c7ceb1975094c
ap3549746 <ap3549746@gmail.com>
2026-06-19 10:17:16 +0530

    RepeatingWord

---
+++ src/day4Loops/RepeatingWord.java
@@ -0,0 +1,12 @@
+package day4Loops;
+public class RepeatingWord {
+    
+    public static void main(String args[]){
+        int counter=0;
+        while(counter<10){
+            System.out.println("Hello World ");
+            counter ++;
+        }
+System.out.println("printed Hello World 10 Times");
+    }
+}
