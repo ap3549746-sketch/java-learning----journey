138eb111fa1c9eddbfef139e8724bd9c06b75d50
ap3549746 <ap3549746@gmail.com>
2026-06-21 14:26:15 +0530

    pattren2

--- src/day5/patterns/pattern2.java
+++ src/day5/patterns/pattern2.java
@@ -1,5 +1,15 @@
+// print right trinage using star patten
+
+
 package day5.patterns;
 
 public class pattern2 {
-    
+    public static void main(String args[]){
+        for (int i=6;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+        }
+        System.out.println( );
+    } 
+}
 }
